8cc2e7a957a255e864413fc66c8a4cff95c7074c
MalshaSamarakoon <73211958+MalshaSamarakoon@users.noreply.github.com>
2020-11-17 19:55:56 +0530

    added all pages

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -78,10 +78,10 @@ li a:hover {
 
             <ul>
   <li><a class="active" href="#home">Home</a></li>
-  <li><a href="#news">News</a></li>
-  <li><a href="#contact">Contact</a></li>
+  <li><a href="/news">News</a></li>
+  <li><a href="/contact">Contact</a></li>
   <li><a href="#Members">Members</a></li>
-  <li><img src="natureclub.png" alt="Italian Trulli"></li>
+  
             </ul>
   </div>
 </nav>
